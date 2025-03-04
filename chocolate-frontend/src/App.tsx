098415ac92d24a0d435584357b0cd99cfc386ce9
joakha <72503684+joakha@users.noreply.github.com>
2025-03-04 15:01:58 +0200

    create frontend project, start designing frontend

---
+++ chocolate-frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import ChocolateHeader from "./components/ChocolateHeader"
+import ChocolateBanner from "./components/ChocolateBanner"
+
+function App() {
+
+  return (
+    <>
+    <ChocolateHeader />
+    <ChocolateBanner />
+    </>
+  )
+}
+
+export default App
