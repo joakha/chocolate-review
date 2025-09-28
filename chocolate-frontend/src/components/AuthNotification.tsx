2ee21f69b648113de74bdcc41e47b9e5fb11cefe
JOAKHA <johannes.haapanen2@gmail.com>
2025-09-28 18:17:21 +0300

    github test

--- chocolate-frontend/src/components/AuthNotification.tsx
+++ chocolate-frontend/src/components/AuthNotification.tsx
@@ -14,7 +14,7 @@ export const AuthNotification = () => {
     }, [closeNotification])
 
     return (
-        <div className="fixed bottom-32 left-4 z-50 p-6 rounded-lg bg-chocolate-light max-w-md">
+        <div className="fixed bottom-32 left-4 z-50 p-6 hello rounded-lg bg-chocolate-light max-w-md">
             <div className="flex justify-center items-center">
                 <p className={`text-2xl font-bold ${notificationMsg?.type === "SUCCESSFUL" ? "text-green-500" : "text-red-500"}`}>{notificationMsg?.msg}</p>
             </div>
