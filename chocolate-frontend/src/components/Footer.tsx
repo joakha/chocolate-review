3b1b8fc60ed0cc25ef9d47af7f5560d5301553d4
joakha <johannes.haapanen2@gmail.com>
2025-10-06 12:20:49 +0300

    footer update + remove redundant readme file

--- chocolate-frontend/src/components/Footer.tsx
+++ chocolate-frontend/src/components/Footer.tsx
@@ -1,11 +1,11 @@
 const Footer = () => {
   return (
     <footer className='py-6 bg-chocolate-dark'>
-        <div className='container mx-auto flex justify-center'>
-            <span className='text-3xl text-white font-bold tracking-tight'>
-                Footer
-            </span>
-        </div>
+      <div className='container mx-auto flex justify-center'>
+        <span className='text-3xl text-white font-bold tracking-tight'>
+          &copy; 2025 Johannes Haapanen
+        </span>
+      </div>
     </footer>
   )
 }
